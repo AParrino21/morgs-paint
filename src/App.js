e70e97fe9a822630df37e770cd284caa1408f7a6
Anthony Parrino <anthony.parrino@yahoo.com>
2021-12-26 20:44:22 -0500

    updated routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-import Home from './pages/Home'
+import Home from './pages/Home';
+import Gallery from './pages/Gallery';
 
 function App() {
   return (
     <div className="App">
-      <Home/>
+      <Router>
+        <Routes>
+          <Route exact path ='/' element = {<Home/>} />
+          <Route exact path ='/gallery' element = {<Gallery/>} />
+        </Routes>
+      </Router>
     </div>
   );
 }
