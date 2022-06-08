a0d40bb3afcb74fcd7eab76936852b0c2a02ce37
Anthony Parrino <anthony21parrino21@gmail.com>
2022-06-08 18:56:47 -0400

    finished styling the shop comp page

--- client/src/components/ShopComp/ShopComp.js
+++ client/src/components/ShopComp/ShopComp.js
@@ -3,6 +3,7 @@ import NavComp from '../NavComp/NavComp'
 import './shopComp.css'
 import Promo1 from './images/promo1.jpg';
 import Promo2 from './images/promo2.jpg';
+import Promo3 from './images/promo3.jpg';
 import PrintCover from '../PrintsComp/images/genisis-prints/gen-print-multi-side.jpg'
 
 
@@ -14,40 +15,36 @@ const ShopComp = () => {
             </div>
 
             <div className='shop-container'>
-                <div className='originals'>
-                    <div onClick={() => window.location.href = '/oilpaintings'}>
-                        <div className='promo-container'>
-                            <img className='promo-imgs' src={Promo1} alt="painting" />
-                        </div>
-                        <br />
-                        <h2 className='shop-titles' onClick={() => window.location.href = '/oilpaintings'}>Oil Paintings</h2>
+                <div onClick={() => window.location.href = '/oilpaintings'}>
+                    <div className='promo-container'>
+                        <img className='promo-imgs' src={Promo1} alt="painting" />
                     </div>
-
-                    <div onClick={() => window.location.href = '/prints'}>
-                        <div className='promo-container'>
-                            <img className='promo-imgs' src={PrintCover} alt="prints" />
-                        </div>
-                        <br />
-                        <h2 className='shop-titles' onClick={() => window.location.href = '/prints'}>Prints</h2>
-                    </div>
+                    <br />
+                    <h2 className='shop-titles' onClick={() => window.location.href = '/oilpaintings'}>Oil Paintings</h2>
                 </div>
 
-                <div className='print-6'>
-                    <div onClick={() => window.location.href = '/mixed-media'}>
-                        <div className='promo-container'>
-                            <img className='promo-imgs' src={Promo2} alt="mixed media" />
-                        </div>
-                        <br />
-                        <h2 className='shop-titles' onClick={() => window.location.href = '/mixed-media'}>Mixed Media</h2>
+                <div onClick={() => window.location.href = '/mixed-media'}>
+                    <div className='promo-container'>
+                        <img className='promo-imgs' src={Promo2} alt="mixed media" />
                     </div>
+                    <br />
+                    <h2 className='shop-titles' onClick={() => window.location.href = '/mixed-media'}>Mixed Media</h2>
+                </div>
 
-                    <div onClick={() => window.location.href = 'https://www.SOCIETY6.com/morgandanton '}>
-                        <div className='promo-container'>
-                            <img className='promo-imgs' src={Promo2} alt="society 6" />
-                        </div>
-                        <br />
-                        <h2 className='shop-titles' onClick={() => window.location.href = 'https://www.SOCIETY6.com/morgandanton '}>Society 6</h2>
+                <div onClick={() => window.location.href = '/prints'}>
+                    <div className='promo-container'>
+                        <img className='promo-imgs' src={PrintCover} alt="prints" />
                     </div>
+                    <br />
+                    <h2 className='shop-titles' onClick={() => window.location.href = '/prints'}>Prints</h2>
+                </div>
+
+                <div onClick={() => window.location.href = 'https://www.SOCIETY6.com/morgandanton '}>
+                    <div className='promo-container'>
+                        <img className='promo-imgs' src={Promo3} alt="society 6" />
+                    </div>
+                    <br />
+                    <h2 className='shop-titles' onClick={() => window.location.href = 'https://www.SOCIETY6.com/morgandanton '}>Society 6</h2>
                 </div>
             </div>
 
